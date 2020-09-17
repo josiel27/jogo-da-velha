9298f5c74f02a0f855eaac841261c0d320590416
Josiel <josielsifa@gmail.com>
2020-09-17 11:04:04 -0300

    Criando funcao para mostrar notificao de empate e funcao para esconder as notificacoes

--- scripts/notifications.js
+++ scripts/notifications.js
@@ -1,9 +1,19 @@
 
 var winNotification = document.getElementById('winNotification');
 var notification = document.getElementById('winnerPlayer');
+var notificationDraw = document.getElementById('drawGame');
+
 
 function showNotification() {
   winNotification.textContent = 'O Player ' + ((turnPlayer === 1) ? "2" : "1") + ' venceu!';
-
   notification.style.display = 'block';
+}
+
+function showNotificationDraw() {
+  notificationDraw.style.display = 'block';
+}
+
+function dismissNotifications() {
+  notification.style.display = 'none';
+  notificationDraw.style.display = 'none';
 }
